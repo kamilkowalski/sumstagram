a95ebbe43a1fd32e85606556b85a0d7b724aa7f1
Kamil Kowalski <kowalski.tychy@gmail.com>
2016-11-13 18:08:36 +0000

    Tworzenie użytkownika

--- solution/app/controllers/users_controller.rb
+++ solution/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def create
+    user_params = params.permit(:username, :email, :password)
+    user = User.new(user_params)
+    
+    if user.valid?
+      user.save
+      render json: {}, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
   end
 end
