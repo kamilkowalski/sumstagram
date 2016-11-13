91e0ac633e2f5ac4e9770d8d40fe897a963b849c
Kamil Kowalski <kowalski.tychy@gmail.com>
2016-11-13 18:07:49 +0000

    Walidacje użytkownika i tokenów

--- solution/app/models/access_token.rb
+++ solution/app/models/access_token.rb
@@ -1,3 +1,6 @@
 class AccessToken < ApplicationRecord
   belongs_to :user
+  
+  validates :code, presence: true, uniqueness: true
+  validates :expires_at, presence: true
 end
--- solution/app/models/user.rb
+++ solution/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-    has_one :access_token
-    has_secure_password
+  has_one :access_token
+  has_secure_password
+  
+  validates :username, :email, presence: true, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
